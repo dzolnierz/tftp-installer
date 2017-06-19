c91a804f4cc568d812bd5aefbad982cc42b4ec9b
Piotr Mazurkiewicz <dzolnierz@gmail.com>
2017-06-19 23:01:48 +0200

    Update to current jessie installer; fix some settings in preseed file

--- scripts/preseed_late_default.sh
+++ scripts/preseed_late_default.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Customize APT
-cat <<-EOF | tee /etc/apt/apt.conf.d/05recommended
-APT::Install-Recommends "false";
+cat <<-EOF | tee /etc/apt/apt.conf.d/00InstallSuggests
 APT::Install-Suggests "false";
 EOF
 
 # Create wheel group
 groupadd -g 11 wheel
 
-# Install needed packages
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" openssh-server sudo lsb-release
+# Install additional packages which they cannot be installed via pkgsel/include
+#DEBIAN_FRONTEND=noninteractive apt-get install -q -y \
+#	-o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
+#	<Packages>
 
 # Customize configs
 sed -i -e 's/^\(PermitRootLogin \).*/\1no/g' /etc/ssh/sshd_config
@@ -24,7 +24,7 @@ EOF
 chmod a-rwx,u+r,g+r /etc/sudoers.d/wheel
 
 # Tune system
-cat <<-EOF | tee /etc/sysctl.d/10-disable-ipv6.conf
+cat <<-EOF | tee /etc/sysctl.d/70-disable-ipv6.conf
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 0
 EOF
@@ -34,10 +34,3 @@ cat <<-EOF | tee /etc/udev/rules.d/80-hotplug-cpu-mem.rules
 SUBSYSTEM=="cpu", ACTION=="add", TEST=="online", ATTR{online}=="0", ATTR{online}="1"
 SUBSYSTEM=="memory", ACTION=="add", TEST=="state", ATTR{state}=="offline", ATTR{state}="online"
 EOF
-
-cat <<-EOF | tee /etc/modules-load.d/hotplug.conf 
-acpiphp
-pci_hotplug
-EOF
-
-# Create users
